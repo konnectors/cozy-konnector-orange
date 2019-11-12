12038f4cb862b1bf29390beabc28d009bb8ccb9c
LucsT <lucas@cozycloud.cc>
2019-11-12 15:29:27 +0100

    fix: Avoid breaking without proxyUrl provided in standalone

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ process.env.SENTRY_DSN =
   'https://913841dc7a6a44a59bb26b70df222286:34e4ff6001884d0fac9091d4c1fee102@sentry.cozycloud.cc/25'
 
 const secrets = JSON.parse(process.env.COZY_PARAMETERS || '{}').secret
-if (secrets.proxyUrl) {
+if (secrets && secrets.proxyUrl) {
   process.env.http_proxy = secrets.proxyUrl
   process.env.https_proxy = secrets.proxyUrl
 }
