5b1629ed7eb9e13d633e2a95ccd378009939abdb
doubleface <christophe@cozycloud.cc>
2020-03-27 12:42:55 +0100

    fix: remove proxy setup at the moment

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ process.env.SENTRY_DSN =
   process.env.SENTRY_DSN ||
   'https://913841dc7a6a44a59bb26b70df222286:34e4ff6001884d0fac9091d4c1fee102@sentry.cozycloud.cc/25'
 
-const secrets = JSON.parse(process.env.COZY_PARAMETERS || '{}').secret
-if (secrets && secrets.proxyUrl) {
-  process.env.http_proxy = secrets.proxyUrl
-  process.env.https_proxy = secrets.proxyUrl
-}
+// const secrets = JSON.parse(process.env.COZY_PARAMETERS || '{}').secret
+// if (secrets && secrets.proxyUrl) {
+//   process.env.http_proxy = secrets.proxyUrl
+//   process.env.https_proxy = secrets.proxyUrl
+// }
 
 const get = require('lodash/get')
 
@@ -31,6 +31,7 @@ class OrangeConnector extends CookieKonnector {
       await this.getContracts()
     } catch (err) {
       log('debug', err.message)
+      this.resetSession()
       log('info', 'Saved session usage failed, connecting')
       return false
     }
