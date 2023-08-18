d3592bb07e8502dcd85b4226a3598705ca507437
Travis CI User <travis@example.org>
2023-08-18 09:03:24 +0000

    publish: WIP Logging
    generated from commit a3ec5f8253e65794ffd36d5e5ce9b1edead8a3e0

--- main.js
+++ main.js
@@ -6340,7 +6340,10 @@ async function getFileName(date, amount, vendorRef) {
 }
 
 async function hashVendorRef(vendorRef) {
+  log('warn', 'Starting hashVendorRef')
+  log('warn', window.location.href)
   const msgUint8 = new window.TextEncoder().encode(vendorRef) // encode as (utf-8) Uint8Array
+  // log('warn', msgUint8)
   const hashBuffer = await window.crypto.subtle.digest('SHA-256', msgUint8) // hash the message
   const hashArray = Array.from(new Uint8Array(hashBuffer)) // convert buffer to byte array
   const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('') // convert bytes to hex string
