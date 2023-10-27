22d058b922f05eb52092a9202867a978f6b06230
doubleface <christophe@cozycloud.cc>
2023-10-27 17:38:52 +0200

    fix: Now fetch bills without interceptions
    File interception did not work on iphone (the file was still displayed
    on on the phone and blocked the rest of the execution of the konnector.
    
    I found a way to download the files directly without clicking on the
    links, which is faster and more reliable. And since downloadInWorker is
    used, file will be downloaded only when needed.

--- src/interceptor.js
+++ src/interceptor.js
@@ -1,8 +1,6 @@
-import { blobToBase64 } from 'cozy-clisk/dist/contentscript/utils'
-
 export default class XhrInterceptor {
   constructor() {
-    this.recentBills = []
+    this.recentBills = {}
     this.oldBills = []
     this.recentPromisesToConvertBlobToBase64 = []
     this.oldPromisesToConvertBlobToBase64 = []
@@ -23,19 +21,8 @@ export default class XhrInterceptor {
       if (arguments[1]?.includes('/users/current/contracts')) {
         originalResponse.addEventListener('readystatechange', function () {
           if (originalResponse.readyState === 4) {
-            // The response is a unique string, in order to access information parsing into JSON is needed.
             const jsonBills = JSON.parse(originalResponse.responseText)
-            self.recentBills.push(jsonBills)
-          }
-        })
-        return proxied.apply(this, [].slice.call(arguments))
-      }
-      // Intercepting response for old bills informations.
-      if (arguments[1]?.includes('/facture/historicBills?')) {
-        originalResponse.addEventListener('readystatechange', function () {
-          if (originalResponse.readyState === 4) {
-            const jsonBills = JSON.parse(originalResponse.responseText)
-            self.oldBills.push(jsonBills)
+            self.recentBills = jsonBills
           }
         })
         return proxied.apply(this, [].slice.call(arguments))
@@ -50,59 +37,28 @@ export default class XhrInterceptor {
         })
         return proxied.apply(this, [].slice.call(arguments))
       }
-      // Intercepting response for recent bills blobs.
-      if (arguments[1]?.includes('facture/v1.0/pdf?billDate')) {
-        originalResponse.addEventListener('readystatechange', function () {
-          if (originalResponse.readyState === 4) {
-            self.recentPromisesToConvertBlobToBase64 = []
-            self.recentXhrUrls = []
-            // Pushing in an array the converted to base64 blob and pushing in another array it's href to match the indexes.
-            self.recentPromisesToConvertBlobToBase64.push(
-              blobToBase64(originalResponse.response)
-            )
-            self.recentXhrUrls.push(originalResponse.__zone_symbol__xhrURL)
 
-            // In every case, always returning the original response untouched
-            return originalResponse
+      // Intercepting more infos for Identity object
+      if (arguments[1]?.includes('ecd_wp/account/identification')) {
+        originalResponse.addEventListener('readystatechange', function () {
+          if (self.originalResponse.readyState === 4) {
+            const jsonInfos = JSON.parse(self.originalResponse.responseText)
+            self.userInfos.push(jsonInfos)
           }
         })
+        return proxied.apply(this, [].slice.call(arguments))
       }
-      // Intercepting response for old bills blobs.
-      if (arguments[1]?.includes('ecd_wp/facture/historicPDF?')) {
+      // Intercepting billingAddress infos for Identity object
+      if (arguments[1]?.includes('ecd_wp/account/billingAddresses')) {
         originalResponse.addEventListener('readystatechange', function () {
-          if (originalResponse.readyState === 4) {
-            self.oldPromisesToConvertBlobToBase64 = []
-            self.oldXhrUrls = []
-            self.oldPromisesToConvertBlobToBase64.push(
-              blobToBase64(originalResponse.response)
-            )
-            self.oldXhrUrls.push(originalResponse.__zone_symbol__xhrURL)
-
-            return originalResponse
+          if (self.originalResponse.readyState === 4) {
+            const jsonInfos = JSON.parse(self.originalResponse.responseText)
+            self.userInfos.push(jsonInfos)
           }
         })
+        return proxied.apply(this, [].slice.call(arguments))
       }
-      return proxied.apply(this, [].slice.call(arguments))
-    }
 
-    // Intercepting more infos for Identity object
-    if (arguments[1]?.includes('ecd_wp/account/identification')) {
-      self.originalResponse.addEventListener('readystatechange', function () {
-        if (self.originalResponse.readyState === 4) {
-          const jsonInfos = JSON.parse(self.originalResponse.responseText)
-          self.userInfos.push(jsonInfos)
-        }
-      })
-      return proxied.apply(this, [].slice.call(arguments))
-    }
-    // Intercepting billingAddress infos for Identity object
-    if (arguments[1]?.includes('ecd_wp/account/billingAddresses')) {
-      self.originalResponse.addEventListener('readystatechange', function () {
-        if (self.originalResponse.readyState === 4) {
-          const jsonInfos = JSON.parse(self.originalResponse.responseText)
-          self.userInfos.push(jsonInfos)
-        }
-      })
       return proxied.apply(this, [].slice.call(arguments))
     }
   }
