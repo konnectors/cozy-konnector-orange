4e3b548270b2305f29578174a16973807c401eef
Travis CI User <travis@example.org>
2024-04-09 08:17:04 +0000

    publish: Merge pull request #644 from konnectors/fix/contractSeparation
    generated from commit d1523b8ebdec19f01d625ab6cbf6c09e421aa6c3

--- main.js
+++ main.js
@@ -6016,6 +6016,7 @@ class OrangeContentScript extends cozy_clisk_dist_contentscript__WEBPACK_IMPORTE
       currentState === 'passwordAlonePage' ||
       currentState === 'mobileConnectPage'
     ) {
+      await this.waitForElementInWorker('[data-testid=change-account]')
       await this.runInWorker('click', '[data-testid=change-account]')
     } else if (currentState === 'captchaPage') {
       await this.handleCaptcha()
@@ -6201,9 +6202,7 @@ class OrangeContentScript extends cozy_clisk_dist_contentscript__WEBPACK_IMPORTE
     }
     await this.goto(DEFAULT_PAGE_URL)
     await this.waitForElementInWorker('.menu')
-
     const contracts = await this.runInWorker('getContracts')
-
     for (const contract of contracts) {
       const { recentBills, oldBillsUrl } = await this.fetchRecentBills(
         contract.vendorId,
@@ -6217,7 +6216,11 @@ class OrangeContentScript extends cozy_clisk_dist_contentscript__WEBPACK_IMPORTE
         qualificationLabel:
           contract.type === 'phone' ? 'phone_invoice' : 'isp_invoice'
       })
-      if (FORCE_FETCH_ALL) {
+      // Due to recent changes in Orange's way to handle contracts
+      // oldbillsUrl might not be present in the intercepted response
+      // Perhaps it will appears differently if it does (when newly created contract will have an history to show)
+      // Fortunately the account we dispose to develop has just been migrated to this new handling so we might be able to do something when it happen
+      if (FORCE_FETCH_ALL && oldBillsUrl) {
         const oldBills = await this.fetchOldBills({
           oldBillsUrl,
           vendorId: contract.vendorId
