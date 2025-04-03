6e4a7f05ff9049983e57cd1d5106f7ce7feb932c
doubleface <christophe@cozycloud.cc>
2025-04-03 18:58:38 +0200

    test: import

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,18 @@
-const path = require('path')
+module.exports = require('cozy-konnector-build/webpack.config.clisk')
+// const path = require('path')
 
-const CopyPlugin = require('copy-webpack-plugin')
+// const CopyPlugin = require('copy-webpack-plugin')
 
-module.exports = {
-  mode: 'none',
-  output: {
-    path: path.join(process.cwd(), 'build'),
-    filename: 'main.js',
-    clean: true
-  },
-  plugins: [
-    new CopyPlugin({
-      patterns: [{ from: 'manifest.konnector' }, { from: 'assets' }]
-    })
-  ]
-}
+// module.exports = {
+//   mode: 'none',
+//   output: {
+//     path: path.join(process.cwd(), 'build'),
+//     filename: 'main.js',
+//     clean: true
+//   },
+//   plugins: [
+//     new CopyPlugin({
+//       patterns: [{ from: 'manifest.konnector' }, { from: 'assets' }]
+//     })
+//   ]
+// }
