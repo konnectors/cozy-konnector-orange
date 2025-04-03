a2bd64735fa664ee911e70ebf45548e6f4f82d82
doubleface <christophe@cozycloud.cc>
2025-04-03 18:50:36 +0200

    fix: Back to old webpack for test

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,16 @@
-module.exports = require('cozy-konnector-build/webpack.config.clisk')
+const path = require('path')
+
+const CopyPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+  mode: 'none',
+  output: {
+    path: path.join(process.cwd(), 'build'),
+    filename: 'main.js'
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: 'manifest.konnector' }, { from: 'assets' }]
+    })
+  ]
+}
