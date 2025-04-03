2c0814356c6e558b2cfde16ccc5565fad0c6e8c6
doubleface <christophe@cozycloud.cc>
2025-04-03 18:54:38 +0200

    test: Try the bad clean option in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,8 @@ module.exports = {
   mode: 'none',
   output: {
     path: path.join(process.cwd(), 'build'),
-    filename: 'main.js'
+    filename: 'main.js',
+    clean: true
   },
   plugins: [
     new CopyPlugin({
