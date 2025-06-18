6475fb5992e410730aa00324b3baec70f83c92df
Killian Courvoisier <killian.courvoisier@cozycloud.cc>
2025-06-18 15:08:52 +0100

    feat: Use promises allSettled function

--- src/index.js
+++ src/index.js
@@ -708,7 +708,8 @@ class OrangeContentScript extends ContentScript {
       includesText: 'Infos de contact'
     })
     await this.runInWorker('click', 'p', { includesText: 'Infos de contact' })
-    await Promise.all([
+    // Using allSettle here because we notice some account did not have adresses registered
+    await Promise.allSettled([
       this.waitForElementInWorker(
         'a[data-e2e="btn-contact-info-modifier-votre-identite"]'
       ),
@@ -716,7 +717,7 @@ class OrangeContentScript extends ContentScript {
         'a[data-e2e="btn-contact-info-phone-modifier"]'
       ),
       this.waitForElementInWorker(
-        'a[data-e2e="btn-contact-info-modifier-vos-adresses-postales"]'
+        'a[data-e2e="btn-contact-info-modifier-vos-adresses-postales"]', {timeout: 5000}
       )
     ])
   }
